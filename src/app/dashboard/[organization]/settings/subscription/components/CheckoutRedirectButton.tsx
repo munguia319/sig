b33cdac0e4a1b73f01aba4da512dbb6fe5d2eaa6
munguia319 <munguia319@gmail.com>
2026-05-13 16:22:10 -0600

    Initialize 0.6.13

---
+++ src/app/dashboard/[organization]/settings/subscription/components/CheckoutRedirectButton.tsx
@@ -0,0 +1,82 @@
+'use client';
+
+import React from 'react';
+import { ChevronRightIcon } from '@heroicons/react/24/outline';
+import classNames from 'clsx';
+
+import Button from '~/core/ui/Button';
+import isBrowser from '~/core/generic/is-browser';
+import useCsrfToken from '~/core/hooks/use-csrf-token';
+import { createCheckoutAction } from '~/lib/stripe/actions';
+
+const CheckoutRedirectButton: React.FCC<{
+  disabled?: boolean;
+  stripePriceId?: string;
+  recommended?: boolean;
+  organizationUid: string;
+  customerId: Maybe<string>;
+}> = ({ children, ...props }) => {
+  return (
+    <form data-cy={'checkout-form'} action={createCheckoutAction}>
+      <CheckoutFormData
+        customerId={props.customerId}
+        organizationUid={props.organizationUid}
+        priceId={props.stripePriceId}
+      />
+
+      <Button
+        block
+        className={classNames({
+          'text-foreground bg-background dark:bg-white dark:text-gray-900':
+            props.recommended,
+        })}
+        variant={props.recommended ? 'custom' : 'secondary'}
+        disabled={props.disabled}
+      >
+        <span className={'flex items-center space-x-2'}>
+          <span>{children}</span>
+
+          <ChevronRightIcon className={'h-4'} />
+        </span>
+      </Button>
+    </form>
+  );
+};
+
+export default CheckoutRedirectButton;
+
+function CheckoutFormData(
+  props: React.PropsWithChildren<{
+    organizationUid: Maybe<string>;
+    priceId: Maybe<string>;
+    customerId: Maybe<string>;
+  }>,
+) {
+  const csrfToken = useCsrfToken();
+
+  return (
+    <>
+      <input
+        type="hidden"
+        name={'organizationUid'}
+        defaultValue={props.organizationUid}
+      />
+
+      <input type="hidden" name={'csrfToken'} defaultValue={csrfToken} />
+      <input type="hidden" name={'returnUrl'} defaultValue={getReturnUrl()} />
+      <input type="hidden" name={'priceId'} defaultValue={props.priceId} />
+
+      <input
+        type="hidden"
+        name={'customerId'}
+        defaultValue={props.customerId}
+      />
+    </>
+  );
+}
+
+function getReturnUrl() {
+  return isBrowser()
+    ? [window.location.origin, window.location.pathname].join('')
+    : undefined;
+}
