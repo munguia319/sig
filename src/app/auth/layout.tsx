94024655514dccf80a953a501adeb383025d490c
munguia319 <munguia319@gmail.com>
2026-05-13 16:20:48 -0600

    Initialize 0.2.8

---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,14 @@
+import { use } from 'react';
+
+import loadAuthPageData from '~/lib/server/loaders/load-auth-page-data';
+import AuthPageShell from '~/app/auth/components/AuthPageShell';
+
+export const dynamic = 'force-dynamic';
+
+async function AuthLayout({ children }: React.PropsWithChildren) {
+  const data = await loadAuthPageData();
+
+  return <AuthPageShell language={data.language}>{children}</AuthPageShell>;
+}
+
+export default AuthLayout;
