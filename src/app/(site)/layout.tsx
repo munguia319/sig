fb5f399892286ed08350dc0114e76a88a3723143
munguia319 <munguia319@gmail.com>
2026-05-13 16:20:33 -0600

    Initialize 0.1.1

---
+++ src/app/(site)/layout.tsx
@@ -0,0 +1,27 @@
+import { use } from 'react';
+
+import Footer from '~/app/(site)/components/Footer';
+import loadUserData from '~/lib/server/loaders/load-user-data';
+import I18nProvider from '~/i18n/I18nProvider';
+import AuthChangeListener from '~/app/(app)/components/AuthChangeListener';
+import SiteHeaderSessionProvider from '~/app/(site)/components/SiteHeaderSessionProvider';
+
+export const dynamic = 'force-dynamic';
+
+function SiteLayout(props: React.PropsWithChildren) {
+  const data = use(loadUserData());
+
+  return (
+    <I18nProvider lang={data.language}>
+      <AuthChangeListener accessToken={data.accessToken}>
+        <SiteHeaderSessionProvider data={data} />
+
+        {props.children}
+
+        <Footer />
+      </AuthChangeListener>
+    </I18nProvider>
+  );
+}
+
+export default SiteLayout;
---
+++ src/core/supabase/middleware-client.ts
@@ -0,0 +1,58 @@
+import { createMiddlewareSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { NextRequest, NextResponse } from 'next/server';
+
+import invariant from 'tiny-invariant';
+import type { Database } from '~/database.types';
+
+/**
+ * Get a Supabase client for use in the Middleware.
+ * @param req
+ * @param res
+ * @param params
+ */
+function getSupabaseMiddlewareClient(
+  req: NextRequest,
+  res: NextResponse,
+  params = {
+    admin: false,
+  }
+) {
+  const env = process.env;
+
+  invariant(env.NEXT_PUBLIC_SUPABASE_URL, `Supabase URL not provided`);
+
+  invariant(
+    env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    `Supabase Anon Key not provided`
+  );
+
+  if (params.admin) {
+    const serviceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+    invariant(serviceRoleKey, `Supabase Service Role Key not provided`);
+
+    return createMiddlewareSupabaseClient<Database>(
+      {
+        req,
+        res,
+      },
+      {
+        supabaseUrl: env.NEXT_PUBLIC_SUPABASE_URL,
+        supabaseKey: serviceRoleKey,
+      }
+    );
+  }
+
+  return createMiddlewareSupabaseClient<Database>(
+    {
+      req,
+      res,
+    },
+    {
+      supabaseUrl: env.NEXT_PUBLIC_SUPABASE_URL,
+      supabaseKey: env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    }
+  );
+}
+
+export default getSupabaseMiddlewareClient;
