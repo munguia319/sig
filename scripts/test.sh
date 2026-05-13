5ff33a96f5cf23a90b348ea21c548053d60fb278
munguia319 <munguia319@gmail.com>
2026-05-13 16:22:02 -0600

    Initialize 0.6.9

---
+++ scripts/test.sh
@@ -0,0 +1,8 @@
+set -e
+
+npm run supabase:start -- -x studio,migra,deno-relay,pgadmin-schema-diff,imgproxy &
+npm run dev:test &
+npm run stripe:mock-server &
+npm run cypress:headless
+npm run supabase:stop -- --no-backup
+exit 0
