9960b489ca74d681eb52b9edcce788c6f480bdf2
munguia319 <munguia319@gmail.com>
2026-05-13 16:22:55 -0600

    Initialize 0.7.12

---
+++ src/lib/user/require-session.ts
@@ -0,0 +1,43 @@
+import { cache } from 'react';
+import { redirect } from 'next/navigation';
+import type { SupabaseClient } from '@supabase/supabase-js';
+
+import verifyRequiresMfa from '~/core/session/utils/check-requires-mfa';
+import configuration from '~/configuration';
+import { Database } from '~/database.types';
+
+/**
+ * @name requireSession
+ * @description Require a session to be present in the request
+ * @param client
+ * @param verifyFromServer
+ */
+const requireSession = cache(
+  async (client: SupabaseClient<Database>, verifyFromServer = true) => {
+    const { data, error } = await client.auth.getSession();
+
+    if (!data.session || error) {
+      return redirect(configuration.paths.signIn);
+    }
+
+    const requiresMfa = await verifyRequiresMfa(client);
+
+    // If the user requires multi-factor authentication,
+    // redirect them to the page where they can verify their identity.
+    if (requiresMfa) {
+      return redirect(configuration.paths.signInMfa);
+    }
+
+    if (verifyFromServer) {
+      const { data: user, error } = await client.auth.getUser();
+
+      if (!user || error) {
+        return redirect(configuration.paths.signIn);
+      }
+    }
+
+    return data.session;
+  },
+);
+
+export default requireSession;
